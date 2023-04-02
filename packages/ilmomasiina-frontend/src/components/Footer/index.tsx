76662cc6ea6fc929a812134ddc4945a619092189
Jonas Bäck <jonas.c.back@gmail.com>
2023-04-02 15:27:05 +0300

    Fix useTranslation call

--- packages/ilmomasiina-frontend/src/components/Footer/index.tsx
+++ packages/ilmomasiina-frontend/src/components/Footer/index.tsx
@@ -9,26 +9,28 @@ import appPaths from '../../paths';
 
 import './Footer.scss';
 
-const { t } = useTranslation();
 
-const Footer = () => (
-  <footer>
-    <Container>
-      <Link to={appPaths.adminEventsList}>
-        {t('admin')}
-      </Link>
-      {branding.footerGdprText && (
-        <a href={branding.footerGdprLink} target="_blank" rel="noreferrer">
-          {branding.footerGdprText}
-        </a>
-      )}
-      {branding.footerHomeText && (
-        <a href={branding.footerHomeLink} target="_blank" rel="noreferrer">
-          {branding.footerHomeText}
-        </a>
-      )}
-    </Container>
-  </footer>
-);
+const Footer = () => {
+  const { t } = useTranslation();
+  return (
+    <footer>
+      <Container>
+        <Link to={appPaths.adminEventsList}>
+          {t('admin')}
+        </Link>
+        {branding.footerGdprText && (
+          <a href={branding.footerGdprLink} target="_blank" rel="noreferrer">
+            {branding.footerGdprText}
+          </a>
+        )}
+        {branding.footerHomeText && (
+          <a href={branding.footerHomeLink} target="_blank" rel="noreferrer">
+            {branding.footerHomeText}
+          </a>
+        )}
+      </Container>
+    </footer>
+  )
+};
 
 export default Footer;
